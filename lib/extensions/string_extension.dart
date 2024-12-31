db2e58a6341dcacf32b99bf627bb066074a8bd05
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-01-01 04:13:18 +0700

    feat: string extension

---
+++ lib/extensions/string_extension.dart
@@ -0,0 +1,44 @@
+extension StringExtension on String {
+  String capitalize() {
+    if (this != '') {
+      return '${this[0].toUpperCase()}${substring(1)}';
+    } else {
+      return '';
+    }
+  }
+
+  bool isNumeric() {
+    return double.tryParse(this) != null;
+  }
+
+  String unescape() {
+    return replaceAll('&amp;', '&').replaceAll('&#039;', "'").replaceAll('&quot;', '"').trim();
+  }
+
+  String formatToHHMMSS() {
+    final int? time = int.tryParse(this);
+    if (time != null) {
+      final int hours = time ~/ 3600;
+      final int seconds = time % 3600;
+      final int minutes = seconds ~/ 60;
+
+      final String hoursStr = hours.toString().padLeft(2, '0');
+      final String minutesStr = minutes.toString().padLeft(2, '0');
+      final String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+
+      if (hours == 0) {
+        return '$minutesStr:$secondsStr';
+      }
+      return '$hoursStr:$minutesStr:$secondsStr';
+    } else {
+      return '';
+    }
+  }
+
+  String get yearFromEpoch => DateTime.fromMillisecondsSinceEpoch(int.parse(this) * 1000).year.toString();
+
+  String get dateFromEpoch {
+    final time = DateTime.fromMillisecondsSinceEpoch(int.parse(this) * 1000);
+    return '${time.day}/${time.month}/${time.year}';
+  }
+}
